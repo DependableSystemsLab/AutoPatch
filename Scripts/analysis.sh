89264651fcf44e0398342a17b9ed66060e788c79
moh3nsalehi <moh3nsalehi@gmail.com>
2023-11-30 12:19:01 -0800

    Scripts

---
+++ Scripts/analysis.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Before using this script, please set the LLVM_BUILD_DIR environment variable.
+# example: export LLVM_BUILD_DIR=~/llvm-project/build
+
+# Variables - modify as needed
+AUTOPATCH_ANALYSIS_REPO=~/test/AutoPatch/AutoPatchSecondPass
+LLVM_PASS_SRC_FOLDER=~/llvm-project/llvm/lib/Transforms/AutoPatch
+# HOTPATCH_DIRECTORY=~/AutoPatch/Testcases/CVE-Lists/Results/Hotpatch_CVE_10063.ll
+
+
+
+# Check if LLVM_BUILD_DIR is set
+if [[ -z "${LLVM_BUILD_DIR}" ]]; then
+    echo "Please set the LLVM_BUILD_DIR environment variable."
+    exit 1
+fi
+
+# Check if input file is provided
+if [[ $# -lt 1 ]]; then
+    echo "Please provide the input LLVM bitcode file."
+    exit 1
+fi
+
+# Define variables
+BC_FILE=$(readlink -f "$1")
+BC_DIR=$(dirname "$BC_FILE")
+BASENAME=$(basename -- "$BC_FILE")
+FILENAME="${BASENAME%.*}"
+LL_FILE="${BC_DIR}/${FILENAME}.ll"
+INST_BC_FILE="${BC_DIR}/Hotpatch.bc"
+LLVMAUTOPATCHSECOND="${LLVM_BUILD_DIR}/lib/LLVMAutoPatchSecond.so"
+OPT="${LLVM_BUILD_DIR}/bin/opt"
+LLVM_AS="${LLVM_BUILD_DIR}/bin/llvm-as"
+
+# Ask user for function patch name and line number patch
+#read -p "Enter function patch name: " funcPatchName
+#read -p "Enter line number patch (separate multiple lines with commas): " lineNumPatchStr
+#IFS=',' read -r -a lineNumPatch <<< "$lineNumPatchStr"
+
+#read -p "Which Type? (filter/replace): " typeOfficialPatch
+
+#if [[ "$typeOfficialPatch" == "filter" ]]; then
+ # type=filter
+#elif [[ "$typeOfficialPatch" == "replace" ]]; then
+  #type=replace
+  #read -p "Enter offset with pc: " offsetPC
+#else
+  #echo "Invalid option: $typeOfficialPatch. Please enter either filter or replace."
+  #exit 1
+#fi
+
+#read -p "Which Type? (redirect/drop/none): " typePatch
+
+#if [[ "$typePatch" == "drop" ]]; then
+ # type=drop
+#elif [[ "$typePatch" == "redirect" ]]; then
+  #type=redirect
+  #read -p "Enter offset with pc: " lineNumPatchStr
+  #IFS=',' read -r -a lineNumPatch <<< "$lineNumPatchStr"
+  #read -p "Enter function patch name (if the patch has): " funcPatchName
+#elif [[ "$typePatch" == "none" ]]; then
+  #type=none
+#else
+  #echo "Invalid option: $typePatch. Please enter either none, redirect or drop."
+  #exit 1
+#fi
+
+read -p "Enter CVE id: " CVEid
+HOTPATCH_DIRECTORY=~/AutoPatch/Testcases/CVE-Lists/Results/Hotpatch_CVE_${CVEid}.ll
+
+
+# Build LLVM with LLVMAutoPatchSecond
+# cp $AUTOPATCH_ANALYSIS_REPO/AutoPatchSecondPass.cpp $LLVM_PASS_SRC_FOLDER/AutoPatchSecondPass/AutoPatchSecondPass.cpp
+cd "${LLVM_BUILD_DIR}"
+ninja
+
+# Record start time
+start=$(date +%s.%N)
+
+# Run Analysis tool on bc file
+#"${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHSECOND}" -time-passes -patching -func-patch-name="${funcPatchName}" -line-num-patch="${lineNumPatchStr}" -output-dir=$HOTPATCH_DIRECTORY -cve-id="${CVEid}" -type-offpatch="${typeOfficialPatch}" -type-patch="${typePatch}" <"${BC_FILE}"> /dev/null
+
+#NEW (AFTER EVALUATION BACK TO THE ABOVE CODE)
+"${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHSECOND}" -O1 -time-passes -patching -func-patch-name=prvCheckOptions -line-num-patch=18,19,20,21 -output-dir=$HOTPATCH_DIRECTORY -cve-id="${CVEid}" -type-offpatch=filter -type-patch=drop <"${BC_FILE}"> /dev/null
+
+
+# Record end time
+end=$(date +%s.%N)
+
+BC_DIR2=$(dirname "${HOTPATCH_DIRECTORY}")
+BASENAME2=$(basename -- "${HOTPATCH_DIRECTORY}")
+INST_BC_FILE2="${BC_DIR2}/${BASENAME2%.*}.bc"
+
+# Now you can compile the ll file to bc file
+"${LLVM_AS}" "${HOTPATCH_DIRECTORY}" -o "${INST_BC_FILE2}"
+
+
+# Compile generated bc file to object file
+CLANG="${LLVM_BUILD_DIR}/bin/clang"
+TARGET="arm-none-eabi"
+MARCH="armv7e-m"
+MCPU="cortex-m4"
+OBJECT_FILE="${BC_DIR2}/${BASENAME2%.*}.o"
+#"$CLANG" "--target=${TARGET}" "-march=${MARCH}" "-mcpu=${MCPU}" "${INST_BC_FILE2}" "-c" "-o" "${OBJECT_FILE}"
+"$CLANG" "--target=${TARGET}" "-march=${MARCH}" "-mcpu=${MCPU}" "${INST_BC_FILE2}" "-c" "-o" "${OBJECT_FILE}" "-Os"
+
+total_time=$(echo "$end - $start" | bc)
+
+echo "Total CPU time taken: ${total_time} seconds"
---
+++ Scripts/instrument.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Before using this script, please set the LLVM_BUILD_DIR environment variable.
+# example: export LLVM_BUILD_DIR=~/llvm-project/build
+
+# Variables - modify as needed
+AUTOPATCH_INSTRUMENT_REPO=~/test/AutoPatch/AutoPatchFirstPass
+LLVM_PASS_SRC_FOLDER=~/llvm-project/llvm/lib/Transforms/AutoPatch
+#HEADER_DIR=~/AutoPatch/zephyr-main/include/
+
+# Check if LLVM_BUILD_DIR is set
+if [[ -z "${LLVM_BUILD_DIR}" ]]; then
+    echo "Please set the LLVM_BUILD_DIR environment variable."
+    exit 1
+fi
+
+# Check if input file is provided
+if [[ $# -lt 1 ]]; then
+    echo "Please provide the input C source file."
+    exit 1
+fi
+
+# Define variables
+SRC_FILE=$(readlink -f "$1")
+SRC_DIR=$(dirname "$SRC_FILE")
+BASENAME=$(basename -- "$SRC_FILE")
+FILENAME="${BASENAME%.*}"
+LL_FILE="${SRC_DIR}/${FILENAME}.ll"
+INST_LL_FILE="${SRC_DIR}/${FILENAME}-inst.ll"
+INST_BC_FILE="${SRC_DIR}/${FILENAME}-inst.bc"
+LLVMAUTOPATCHFIRST="${LLVM_BUILD_DIR}/lib/LLVMAutoPatchFirst.so"
+OPT="${LLVM_BUILD_DIR}/bin/opt"
+LLVM_AS="${LLVM_BUILD_DIR}/bin/llvm-as"
+
+read -p "Is it patched? (true/false): " isPatchedInput
+
+if [[ "$isPatchedInput" == "false" ]]; then
+  isPatched=false
+else
+  isPatched=true
+  read -p "Enter line number patch (separate multiple lines with commas): " lineNumPatchStr
+  IFS=',' read -r -a lineNumPatch <<< "$lineNumPatchStr"
+  read -p "Enter function patch name (if the patch has): " funcPatchName
+fi
+
+
+
+
+# Compile C code to ll file
+# clang -O0 -g -S -emit-llvm -I"${HEADER_DIR}" -o "${LL_FILE}" -c "${SRC_FILE}"
+clang -O0 -g -S -emit-llvm -o "${LL_FILE}" -c "${SRC_FILE}"
+
+# Build LLVM with LLVMAutoPatchFirst
+# cp $AUTOPATCH_INSTRUMENT_REPO/AutoPatchFirstPass.cpp $LLVM_PASS_SRC_FOLDER/AutoPatchFirstPass/AutoPatchFirstPass.cpp
+cd "${LLVM_BUILD_DIR}"
+ninja
+
+# echo "HEADER_DIR=${HEADER_DIR}"
+
+# Record start time
+start=$(date +%s.%N)
+
+# Run Instrumentation tool on ll file
+# "${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHFIRST}" -instrumentcode -I"${HEADER_DIR}" "${LL_FILE}" -o "${INST_LL_FILE}"
+# "${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHFIRST}" -instrumentcode -is-patched="${isPatchedInput}" -line-num-patch="${lineNumPatchStr}" -func-patch-name="${funcPatchName}" "${LL_FILE}" -o "${INST_LL_FILE}"
+
+# Pass the --line-num-patch option only if isPatchedInput is true and lineNumPatchStr is not empty
+if [[ "$isPatched" == "true" && -n "$lineNumPatchStr" ]]; then
+  "${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHFIRST}" -time-passes -instrumentcode -is-patched="${isPatchedInput}" -line-num-patch="${lineNumPatchStr}" -func-patch-name="${funcPatchName}" "${LL_FILE}" -o "${INST_LL_FILE}"
+else
+  "${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHFIRST}" -time-passes -instrumentcode -is-patched="${isPatchedInput}" -func-patch-name="${funcPatchName}" "${LL_FILE}" -o "${INST_LL_FILE}"
+fi
+#Record end time
+end=$(date +%s.%N)
+
+total_time=$(echo "$end - $start" | bc)
+
+
+# Compile generated ll file by IC to bc
+"${LLVM_AS}" "${INST_LL_FILE}" -o "${INST_BC_FILE}"
+
+
+echo "Total CPU time taken: ${total_time} seconds"
