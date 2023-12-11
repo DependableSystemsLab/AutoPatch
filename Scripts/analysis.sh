219c615e438ea7a64d1ba50ee20154f7fa367ea7
JerryShao3234 <shaojerry@icloud.com>
2023-12-10 22:04:44 -0800

    Make analysis.sh accept repo struct

--- Scripts/analysis.sh
+++ Scripts/analysis.sh
@@ -4,15 +4,17 @@
 # example: export LLVM_BUILD_DIR=~/llvm-project/build
 
 # Variables - modify as needed
-AUTOPATCH_ANALYSIS_REPO=~/test/AutoPatch/AutoPatchSecondPass
-LLVM_PASS_SRC_FOLDER=~/llvm-project/llvm/lib/Transforms/AutoPatch
-# HOTPATCH_DIRECTORY=~/AutoPatch/Testcases/CVE-Lists/Results/Hotpatch_CVE_10063.ll
-
-
+LLVM_PASS_SRC_FOLDER=LLVM_BUILD_DIR/../llvm/lib/Transforms/AutoPatch
 
 # Check if LLVM_BUILD_DIR is set
 if [[ -z "${LLVM_BUILD_DIR}" ]]; then
     echo "Please set the LLVM_BUILD_DIR environment variable."
+    exit 1
+fi
+
+# Check if AutoPatchCode repo is cloned
+if [[ -z "${AUTOPATCHCODE_DIR}" ]]; then
+    echo "Please set the AUTOPATCHCODE_DIR environment variable."
     exit 1
 fi
 
@@ -34,41 +36,42 @@ OPT="${LLVM_BUILD_DIR}/bin/opt"
 LLVM_AS="${LLVM_BUILD_DIR}/bin/llvm-as"
 
 # Ask user for function patch name and line number patch
-#read -p "Enter function patch name: " funcPatchName
-#read -p "Enter line number patch (separate multiple lines with commas): " lineNumPatchStr
-#IFS=',' read -r -a lineNumPatch <<< "$lineNumPatchStr"
+read -p "Enter function patch name: " funcPatchName
+read -p "Enter line number patch (separate multiple lines with commas): " lineNumPatchStr
+IFS=',' read -r -a lineNumPatch <<< "$lineNumPatchStr"
 
-#read -p "Which Type? (filter/replace): " typeOfficialPatch
+read -p "Which Type? (filter/replace): " typeOfficialPatch
 
-#if [[ "$typeOfficialPatch" == "filter" ]]; then
- # type=filter
-#elif [[ "$typeOfficialPatch" == "replace" ]]; then
-  #type=replace
-  #read -p "Enter offset with pc: " offsetPC
-#else
-  #echo "Invalid option: $typeOfficialPatch. Please enter either filter or replace."
-  #exit 1
-#fi
+if [[ "$typeOfficialPatch" == "filter" ]]; then
+ type=filter
+elif [[ "$typeOfficialPatch" == "replace" ]]; then
+  type=replace
+  read -p "Enter offset with pc: " offsetPC
+else
+  echo "Invalid option: $typeOfficialPatch. Please enter either filter or replace."
+  exit 1
+fi
 
-#read -p "Which Type? (redirect/drop/none): " typePatch
+read -p "Which Type? (redirect/drop/none): " typePatch
 
-#if [[ "$typePatch" == "drop" ]]; then
- # type=drop
-#elif [[ "$typePatch" == "redirect" ]]; then
-  #type=redirect
-  #read -p "Enter offset with pc: " lineNumPatchStr
-  #IFS=',' read -r -a lineNumPatch <<< "$lineNumPatchStr"
-  #read -p "Enter function patch name (if the patch has): " funcPatchName
-#elif [[ "$typePatch" == "none" ]]; then
-  #type=none
-#else
-  #echo "Invalid option: $typePatch. Please enter either none, redirect or drop."
-  #exit 1
-#fi
+if [[ "$typePatch" == "drop" ]]; then
+ type=drop
+elif [[ "$typePatch" == "redirect" ]]; then
+  type=redirect
+  read -p "Enter offset with pc: " lineNumPatchStr
+  IFS=',' read -r -a lineNumPatch <<< "$lineNumPatchStr"
+  read -p "Enter function patch name (if the patch has): " funcPatchName
+elif [[ "$typePatch" == "none" ]]; then
+  type=none
+else
+  echo "Invalid option: $typePatch. Please enter either none, redirect or drop."
+  exit 1
+fi
 
 read -p "Enter CVE id: " CVEid
-HOTPATCH_DIRECTORY=~/AutoPatch/Testcases/CVE-Lists/Results/Hotpatch_CVE_${CVEid}.ll
-
+read -p "Enter CVE year: " CVEyear
+#HOTPATCH_DIRECTORY=~/workspace/DependableSystemsLab/AutoPatchCode/Testcases/CVE-${CVEyear}-${CVEid}'(auto)'/patchedFunc.ll
+HOTPATCH_DIRECTORY=${AUTOPATCHCODE_DIR}/Testcases/CVE-${CVEyear}-${CVEid}'(auto)'/patchedFunc.ll
 
 # Build LLVM with LLVMAutoPatchSecond
 # cp $AUTOPATCH_ANALYSIS_REPO/AutoPatchSecondPass.cpp $LLVM_PASS_SRC_FOLDER/AutoPatchSecondPass/AutoPatchSecondPass.cpp
@@ -82,7 +85,7 @@ start=$(date +%s.%N)
 #"${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHSECOND}" -time-passes -patching -func-patch-name="${funcPatchName}" -line-num-patch="${lineNumPatchStr}" -output-dir=$HOTPATCH_DIRECTORY -cve-id="${CVEid}" -type-offpatch="${typeOfficialPatch}" -type-patch="${typePatch}" <"${BC_FILE}"> /dev/null
 
 #NEW (AFTER EVALUATION BACK TO THE ABOVE CODE)
-"${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHSECOND}" -O1 -time-passes -patching -func-patch-name=prvCheckOptions -line-num-patch=18,19,20,21 -output-dir=$HOTPATCH_DIRECTORY -cve-id="${CVEid}" -type-offpatch=filter -type-patch=drop <"${BC_FILE}"> /dev/null
+"${OPT}" -S -enable-new-pm=0 -load "${LLVMAUTOPATCHSECOND}" -O1 -time-passes -patching -func-patch-name="${funcPatchName}" -line-num-patch="${lineNumPatchStr}" -output-dir=$HOTPATCH_DIRECTORY -cve-id="${CVEid}" -type-offpatch=filter -type-patch=${typePatch}" <"${BC_FILE}"> /dev/null
 
 
 # Record end time
