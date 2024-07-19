74fd1a28731a6a93bbd2d481af01319ac33dff4a
Mohsen Salehi <moh3nsalehi@gmail.com>
2024-07-19 16:30:14 -0700

    Edit PCEvaluation

--- PCEvaluation/Script.sh
+++ PCEvaluation/Script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+echo "Welcome to AutoPatch Artifact"
+echo "Compiling main program to bytecode ..."
 clang -emit-llvm -c test.c -o test.bc
+echo "Lininkg main and generated hotpatch ..." 
 llvm-link test.bc Hotpatch_CVE_10021.bc -o linked.bc
 llc -filetype=obj linked.bc -o linked.o
+echo "Generating executable file ..."
 clang linked.o -o my_program
+echo "Executing the final program"
 ./my_program
+echo "Congratulations! :) Process is successful!"
--- PCEvaluation/Script_withoutlink.sh
+++ PCEvaluation/Script_withoutlink.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
+echo "Welcome to AutoPatch Artifact"
+echo "Compiling main program to bytecode ..."
 clang -emit-llvm -c test.c -o test.bc
-
 # llvm-link test.bc Hotpatch_CVE_10021.bc -o linked.bc
-
 llc -filetype=obj test.bc -o linked.o
-
+echo "Generating executable file ..."
 clang linked.o -o my_program
-
 ./my_program
+
